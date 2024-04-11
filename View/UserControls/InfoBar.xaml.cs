99ecb2da088b4ffdec8429d38a69563f62cd1031
Morfinix <morfinic@proton.me>
2024-04-11 13:52:41 +0200

    Add project files.

---
+++ View/UserControls/InfoBar.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace PWR_VI_PodPro.View.UserControls
+{
+    /// <summary>
+    /// Interaction logic for InfoBar.xaml
+    /// </summary>
+    public partial class InfoBar : UserControl
+    {
+        public InfoBar()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ View/UserControls/ProgramBar.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace PWR_VI_PodPro.View.UserControls
+{
+    public partial class ProgramBar : UserControl
+    {
+        public ProgramBar()
+        {
+            InitializeComponent();
+        }
+
+        private void dragBar_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            Window.GetWindow(this).DragMove();
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.WindowState = WindowState.Minimized;
+        }
+    }
+}
