f153c621871aeb515690e97b27f7955b34fbf653
Morfinix <morfinic@proton.me>
2024-06-14 21:35:21 +0200

    Zaktualizowano dokumentację

--- docs/search/all_5.js
+++ docs/search/all_5.js
@@ -1,5 +1,8 @@
 var searchData=
 [
   ['filtereditorview_0',['FilterEditorView',['../class_p_w_r___v_i___pod_pro_1_1_view_1_1_user_controls_1_1_filter_editor_view.html',1,'PWR_VI_PodPro::View::UserControls']]],
-  ['filterview_1',['FilterView',['../class_p_w_r___v_i___pod_pro_1_1_view_1_1_user_controls_1_1_filter_view.html',1,'PWR_VI_PodPro::View::UserControls']]]
+  ['filtermodel_1',['FilterModel',['../class_p_w_r___v_i___pod_pro_1_1_core_1_1_mongo_d_b_1_1_models_1_1_filter_model.html',1,'PWR_VI_PodPro::Core::MongoDB::Models']]],
+  ['filterobj_2',['filterObj',['../class_p_w_r___v_i___pod_pro_1_1_core_1_1filter_obj.html',1,'PWR_VI_PodPro::Core']]],
+  ['filterview_3',['FilterView',['../class_p_w_r___v_i___pod_pro_1_1_view_1_1_user_controls_1_1_filter_view.html',1,'PWR_VI_PodPro::View::UserControls']]],
+  ['filterwindow_4',['FilterWindow',['../class_p_w_r___v_i___pod_pro_1_1_view_1_1_components_1_1_filter_window.html',1,'PWR_VI_PodPro::View::Components']]]
 ];
--- docs/search/all_a.js
+++ docs/search/all_a.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['main_0',['Main',['../class_p_w_r___v_i___pod_pro_1_1_app.html#ace5c342b9ca33ed571d9129a10230336',1,'PWR_VI_PodPro.App.Main()'],['../class_p_w_r___v_i___pod_pro_1_1_app.html#ace5c342b9ca33ed571d9129a10230336',1,'PWR_VI_PodPro.App.Main()']]],
+  ['main_0',['Main',['../class_p_w_r___v_i___pod_pro_1_1_app.html#ace5c342b9ca33ed571d9129a10230336',1,'PWR_VI_PodPro.App.Main()'],['../class_p_w_r___v_i___pod_pro_1_1_app.html#ace5c342b9ca33ed571d9129a10230336',1,'PWR_VI_PodPro.App.Main()'],['../class_p_w_r___v_i___pod_pro_1_1_app.html#ace5c342b9ca33ed571d9129a10230336',1,'PWR_VI_PodPro.App.Main()']]],
   ['mainviewmodel_1',['MainViewModel',['../class_p_w_r___v_i___pod_pro_1_1_view_1_1_view_model_1_1_main_view_model.html',1,'PWR_VI_PodPro::View::ViewModel']]],
   ['mainwindow_2',['MainWindow',['../class_p_w_r___v_i___pod_pro_1_1_main_window.html',1,'PWR_VI_PodPro']]],
   ['manalertbtn_5fclick_3',['ManAlertbtn_Click',['../class_p_w_r___v_i___pod_pro_1_1_view_1_1_user_controls_1_1_settings_view.html#ae50df0d59c5ee1212226e87e43fda865',1,'PWR_VI_PodPro::View::UserControls::SettingsView']]],
